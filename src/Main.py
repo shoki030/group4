7d66faf1157e94e6ea5d589575e220c35ae3298f
Kosuke Asaka <e185405@ie.u-ryukyu.ac.jp>
2020-06-18 15:54:40 +0900

    third commit mikansei

--- src/Main.py
+++ src/Main.py
@@ -47,3 +47,119 @@ del z, imgfiles
 
 print(X.shape, Y.shape)
 
+# trainデータとtestデータに分割
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    Y,
+    random_state = 0,
+    test_size = 0.2
+)
+del X,Y
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
+
+# データ型の変換＆正規化
+X_train = X_train.astype('float32') / 255
+X_test = X_test.astype('float32') / 255
+
+# trainデータからvalidデータを分割
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_train,
+    y_train,
+    random_state = 0,
+    test_size = 0.2
+)
+print(X_train.shape, y_train.shape, X_valid.shape, y_valid.shape)
+
+base_model = Xception(
+    include_top = False,
+    weights = "imagenet",
+    input_shape = None
+)
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(1)(x)
+
+datagen = ImageDataGenerator(
+    featurewise_center = False,
+    samplewise_center = False,
+    featurewise_std_normalization = False,
+    samplewise_std_normalization = False,
+    zca_whitening = False,
+    rotation_range = 0,
+    width_shift_range = 0.1,
+    height_shift_range = 0.1,
+    horizontal_flip = True,
+    vertical_flip = False
+)
+
+# EarlyStopping
+early_stopping = EarlyStopping(
+    monitor = 'val_loss',
+    patience = 10,
+    verbose = 1
+)
+
+# ModelCheckpoint
+weights_dir = './weights/'
+if os.path.exists(weights_dir) == False:os.mkdir(weights_dir)
+model_checkpoint = ModelCheckpoint(
+    weights_dir + "val_loss{val_loss:.3f}.hdf5",
+    monitor = 'val_loss',
+    verbose = 1,
+    save_best_only = True,
+    save_weights_only = True,
+    period = 3
+)
+
+# reduce learning rate
+reduce_lr = ReduceLROnPlateau(
+    monitor = 'val_loss',
+    factor = 0.1,
+    patience = 3,
+    verbose = 1
+)
+
+# log for TensorBoard
+logging = TensorBoard(log_dir = "log/")
+
+# RMSE
+from keras import backend as K
+def root_mean_squared_error(y_true, y_pred):
+        return K.sqrt(K.mean(K.square(y_pred - y_true), axis = -1))
+
+# ネットワーク定義
+model = Model(inputs = base_model.input, outputs = predictions)
+
+#108層までfreeze
+for layer in model.layers[:108]:
+    layer.trainable = False
+
+    # Batch Normalizationのfreeze解除
+    if layer.name.startswith('batch_normalization'):
+        layer.trainable = True
+    if layer.name.endswith('bn'):
+        layer.trainable = True
+
+#109層以降、学習させる
+for layer in model.layers[108:]:
+    layer.trainable = True
+
+# layer.trainableの設定後にcompile
+model.compile(
+    optimizer = Adam(),
+    loss = root_mean_squared_error,
+)
+
+
+hist = model.fit(
+    datagen.flow(X_train, y_train, batch_size = 32),
+    steps_per_epoch = X_train.shape[0] // 32,
+    epochs = 50,
+    validation_data = (X_valid, y_valid)
+    #callbacks = [early_stopping, reduce_lr],
+    #shuffle = True
+    #verbose = 1
+)
